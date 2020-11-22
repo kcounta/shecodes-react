b146811b30ae6fea62361bff2bd0545e4f713265
katerinacounta <katerina.counta@gmail.com>
2020-11-22 19:15:10 +0200

    remove loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import Loader from 'react-loader-spinner';
 
 export default function Search() {
   const [city, setCity] = useState("");
